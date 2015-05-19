c52702d201174b5ee67e53dfc765f141200ae417
A. Caroline King <acarolineking@gmail.com>
2015-05-19 20:41:19 +0200

    Add custom width

--- grid.js
+++ grid.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
-	for(var i=0; i<9; i++) {
+	var $width = 3;
+
+	for(var i=0; i<$width*$width; i++) {
 		$("#container").append("<div class='grid'></div>");
 	};
 
-	$(".grid").height("200px");
-	$(".grid").width("200px");
-
 	$(".grid").mouseenter(function(){
 		$(this).css("background-color", "#000");
+	});
+
+
+	$("button[name='clear']").click(function(){
+		$("#container").empty();
+		$width = prompt("Please put width of the grid:")
+		for(var i=0; i<$width*$width; i++) {
+			$("#container").append("<div class='grid'></div>");
+		};
+		$(".grid").height(600/$width + "px");
+		$(".grid").width(600/$width + "px");
+
+		$(".grid").mouseenter(function(){
+			$(this).css("background-color", "#000");
+		});
 	});
 });
