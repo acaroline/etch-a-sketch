c5d7484a94dd75d299502cdbf0eab64d34cfc270
A. Caroline King <acarolineking@gmail.com>
2015-05-19 21:09:16 +0200

    Add colours choose

--- grid.js
+++ grid.js
@@ -12,15 +12,25 @@ $(document).ready(function(){
 
 	$("button[name='clear']").click(function(){
 		$("#container").empty();
-		$width = prompt("Please put width of the grid:")
+		$width = prompt("Please put width of the grid:");
+
 		for(var i=0; i<$width*$width; i++) {
 			$("#container").append("<div class='grid'></div>");
 		};
+		
 		$(".grid").height(600/$width + "px");
 		$(".grid").width(600/$width + "px");
 
-		$(".grid").mouseenter(function(){
-			$(this).css("background-color", "#000");
-		});
+		colorChange("#000");
 	});
+
+	$("button[name='color']").click(function(){
+		colorChange($(this).val());
+	});
+
+	var colorChange = function(color) {
+		$(".grid").mouseenter(function(){
+			$(this).css("background-color", color);
+		});
+	};
 });
